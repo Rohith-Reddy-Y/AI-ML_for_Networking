a6bd878527b1096337204a5147e5580d310f74d5
Rohith-Reddy-Y <rohithreddyrry2004@gmail.com>
2025-06-19 23:56:37 +0530

    trained sqli and xss models with huge datasets

--- ML-XSS-Detection-main/ml_xss.py
+++ ML-XSS-Detection-main/ml_xss.py
@@ -1,165 +1,92 @@
-#
-from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from gensim.models.doc2vec import Doc2Vec
 from nltk.tokenize import word_tokenize
-from sklearn import model_selection
-from sklearn.linear_model import LogisticRegression
 from urllib.parse import unquote
 import numpy as np
-import pandas as pd
-import csv
-import urllib.parse as parse
 import pickle
-import sys
+import base64
+import xml.etree.ElementTree as ET
 
-np.random.seed(42)
+# Load models
+model = Doc2Vec.load("lib/d2v.model")
+models = {
+    "DecisionTree": pickle.load(open("lib/DecisionTreeClassifier.sav", "rb")),
+    "SVC": pickle.load(open("lib/SVC.sav", "rb")),
+    "GaussianNB": pickle.load(open("lib/GaussianNB.sav", "rb")),
+    "KNN": pickle.load(open("lib/KNeighborsClassifier.sav", "rb")),
+    "RandomForest": pickle.load(open("lib/RandomForestClassifier.sav", "rb")),
+    "MLP": pickle.load(open("lib/MLPClassifier.sav", "rb"))
+}
 
-filename1 = 'lib/DecisionTreeClassifier.sav'
-filename2 = 'lib/SVC.sav'
-filename3 = 'lib/GaussianNB.sav'
-filename4 = 'lib/KNeighborsClassifier.sav'
-filename5 = 'lib/RandomForestClassifier.sav'
-filename6 = 'lib/MLPClassifier.sav'
-# load the models from disk
-loaded_model1 = pickle.load(open(filename1, 'rb'))
-loaded_model2 = pickle.load(open(filename2, 'rb'))
-loaded_model3 = pickle.load(open(filename3, 'rb'))
-loaded_model4 = pickle.load(open(filename4, 'rb'))
-loaded_model5 = pickle.load(open(filename5, 'rb'))
-loaded_model6 = pickle.load(open(filename6, 'rb'))
-model= Doc2Vec.load("lib/d2v.model")
+weights = {
+    "DecisionTree": 0.175,
+    "SVC": 0.15,
+    "GaussianNB": 0.05,
+    "KNN": 0.075,
+    "RandomForest": 0.25,
+    "MLP": 0.3
+}
 
-# Create a function to convert an array of strings to a set of features
-def getVec(text):
-    features = []
-    for i, line in enumerate(text):
-        test_data = word_tokenize(line.lower())
-        v1 = model.infer_vector(test_data)
-        #print("V1_infer", v1)
-        featureVec = v1
-        #print(featureVec)
-        lineDecode = unquote(line)
-        lowerStr = str(lineDecode).lower()
-        #print("X "+str(i)+"=> "+lowerStr)
-        # add feature for malicious tag count
-        feature1 = int(lowerStr.count('link'))
-        feature1 += int(lowerStr.count('object'))
-        feature1 += int(lowerStr.count('form'))
-        feature1 += int(lowerStr.count('embed'))
-        feature1 += int(lowerStr.count('ilayer'))
-        feature1 += int(lowerStr.count('layer'))
-        feature1 += int(lowerStr.count('style'))
-        feature1 += int(lowerStr.count('applet'))
-        feature1 += int(lowerStr.count('meta'))
-        feature1 += int(lowerStr.count('img'))
-        feature1 += int(lowerStr.count('iframe'))
-        feature1 += int(lowerStr.count('marquee'))
-        # add feature for malicious method count
-        feature2 = int(lowerStr.count('exec'))
-        feature2 += int(lowerStr.count('fromcharcode'))
-        feature2 += int(lowerStr.count('eval'))
-        feature2 += int(lowerStr.count('alert'))
-        feature2 += int(lowerStr.count('getelementsbytagname'))
-        feature2 += int(lowerStr.count('write'))
-        feature2 += int(lowerStr.count('unescape'))
-        feature2 += int(lowerStr.count('escape'))
-        feature2 += int(lowerStr.count('prompt'))
-        feature2 += int(lowerStr.count('onload'))
-        feature2 += int(lowerStr.count('onclick'))
-        feature2 += int(lowerStr.count('onerror'))
-        feature2 += int(lowerStr.count('onpage'))
-        feature2 += int(lowerStr.count('confirm'))
-        # add feature for ".js" count
-        feature3 = int(lowerStr.count('.js'))
-        # add feature for "javascript" count
-        feature4 = int(lowerStr.count('javascript'))
-        # add feature for length of the string
-        feature5 = int(len(lowerStr))
-        # add feature for "<script"  count
-        feature6 = int(lowerStr.count('script'))
-        feature6 += int(lowerStr.count('<script'))
-        feature6 += int(lowerStr.count('&lt;script'))
-        feature6 += int(lowerStr.count('%3cscript'))
-        feature6 += int(lowerStr.count('%3c%73%63%72%69%70%74'))
-        # add feature for special character count
-        feature7 = int(lowerStr.count('&'))
-        feature7 += int(lowerStr.count('<'))
-        feature7 += int(lowerStr.count('>'))
-        feature7 += int(lowerStr.count('"'))
-        feature7 += int(lowerStr.count('\''))
-        feature7 += int(lowerStr.count('/'))
-        feature7 += int(lowerStr.count('%'))
-        feature7 += int(lowerStr.count('*'))
-        feature7 += int(lowerStr.count(';'))
-        feature7 += int(lowerStr.count('+'))
-        feature7 += int(lowerStr.count('='))
-        feature7 += int(lowerStr.count('%3C'))
-        # add feature for http count
-        feature8 = int(lowerStr.count('http'))
-        
-        # append the features
-        featureVec = np.append(featureVec,feature1)
-        featureVec = np.append(featureVec,feature2)
-        featureVec = np.append(featureVec,feature3)
-        featureVec = np.append(featureVec,feature4)
-        featureVec = np.append(featureVec,feature5)
-        featureVec = np.append(featureVec,feature6)
-        featureVec = np.append(featureVec,feature7)
-        featureVec = np.append(featureVec,feature8)
-        #print(featureVec)
-        features.append(featureVec)
-        #print(features)
-    return features
+def extract_xss_features(text):
+    tokens = word_tokenize(text.lower())
+    vec = model.infer_vector(tokens)
+    feature_vec = vec
 
+    # Tags
+    tags = ['link','object','form','embed','ilayer','layer','style','applet','meta','img','iframe','marquee']
+    tag_count = sum(text.count(t) for t in tags)
 
+    # JS methods
+    methods = ['exec','fromcharcode','eval','alert','getelementsbytagname','write','unescape','escape','prompt','onload','onclick','onerror','onpage','confirm']
+    method_count = sum(text.count(m) for m in methods)
 
-f = open('lib/test.txt', 'r')
-testXSS = f.readlines()
-Xnew = getVec(testXSS)
-f.close()
-print("")
+    js_count = text.count('.js')
+    javascript_count = text.count('javascript')
+    length = len(text)
 
-# make a prediction
-#1 DecisionTreeClassifier
-ynew1 = loaded_model1.predict(Xnew)
-#2 SVC
-ynew2 = loaded_model2.predict(Xnew)
-#3 GaussianNB
-ynew3 = loaded_model3.predict(Xnew)
-#4 KNeighborsClassifier
-ynew4 = loaded_model4.predict(Xnew)
-#5 RandomForestClassifier
-ynew5 = loaded_model5.predict(Xnew)
-#6 MLPClassifier
-ynew6 = loaded_model6.predict(Xnew)
+    script_count = text.count('script') + text.count('<script') + text.count('&lt;script') + text.count('%3cscript') + text.count('%3c%73%63%72%69%70%74')
 
-# show the sample inputs and predicted outputs
-xssCount = 0 
-notXssCount = 0
-for i in range(len(Xnew)):
-    score = ((.175*ynew1[i])+(.15*ynew2[i])+(.05*ynew3[i])+(.075*ynew4[i])+(.25*ynew5[i])+(.3*ynew6[i]))
-    #print(ynew1[i])
-    #print(ynew2[i])
-    #print(ynew3[i])
-    #print(ynew4[i])
-    #print(ynew5[i])
-    #print(ynew6[i])
-    print()
-    print(score)
-    if score >= .5:
-        print("\033[1;31;1mXSS\033[0;0m => "+testXSS[i])
-        xssCount += 1
+    special_count = sum(text.count(c) for c in ['&', '<', '>', '"', "'", '/', '%', '*', ';', '+', '=', '%3C'])
+
+    http_count = text.count('http')
+
+    feature_vec = np.append(feature_vec, [tag_count, method_count, js_count, javascript_count, length, script_count, special_count, http_count])
+    return feature_vec
+
+def parse_burp(file):
+    results = []
+    try:
+        tree = ET.parse(file)
+        root = tree.getroot()
+        for item in root.findall('item'):
+            request_data = item.findtext('request', default="")
+            is_base64 = item.find('request').attrib.get('base64', 'false') == 'true'
+            raw = base64.b64decode(request_data).decode(errors='ignore') if is_base64 else request_data
+
+            # Parse HTTP Request
+            lines = raw.splitlines()
+            first_line = lines[0] if lines else ""
+            if ' ' in first_line:
+                method, path, *_ = first_line.split()
+                results.append(path)
+    except Exception as e:
+        print("Error parsing log:", e)
+    return results
+
+testXSS = parse_burp("bad_requests.log")
+
+X = [extract_xss_features(unquote(line.lower())) for line in testXSS]
+
+xss_count, not_xss_count = 0, 0
+
+for i, x in enumerate(X):
+    score = sum(models[name].predict([x])[0] * weight for name, weight in weights.items())
+    if score >= 0.5:
+        print(f"\033[1;31;1mXSS\033[0;0m => {testXSS[i]}")
+        xss_count += 1
     else:
-        #print(ynew1[i])
-        #print(ynew2[i])
-        #print(ynew3[i])
-        #print(ynew4[i])
-        #print(ynew5[i])
-        #print(ynew6[i])
-        #print(score)
-        print("\033[1;32;1mNOT XSS\033[0;0m => "+testXSS[i])
-        notXssCount += 1
+        print(f"\033[1;32;1mNOT XSS\033[0;0m => {testXSS[i]}")
+        not_xss_count += 1
 
-print()
-print("*------------- RESULTS -------------*")
-print("\033[1;31;1mXSS\033[0;0m => "+str(xssCount))
-print("\033[1;32;1mNOT XSS\033[0;0m => "+str(notXssCount))
+print("\n*------------- RESULTS -------------*")
+print(f"\033[1;31;1mXSS\033[0;0m => {xss_count}")
+print(f"\033[1;32;1mNOT XSS\033[0;0m => {not_xss_count}")
--- real_time_detection/real_time_detection.py
+++ real_time_detection/real_time_detection.py
@@ -1,3 +1,4 @@
+
 import base64
 from urllib.parse import unquote, unquote_plus
 from flask import Flask, request, render_template, jsonify
@@ -12,186 +13,197 @@ from gensim.models.doc2vec import Doc2Vec
 from nltk.tokenize import word_tokenize
 import nltk
 import os
-
+import chardet
+import re
+import warnings
+# Suppress version warnings temporarily for clarity
+warnings.filterwarnings("ignore", category=UserWarning)
 # Download NLTK data
-nltk.download('punkt')
-
+try:
+    nltk.download('punkt')
+    nltk.download('punkt_tab')
+except Exception as e:
+    print(f"Error downloading NLTK data: {e}")
+    exit(1)
 app = Flask(__name__)
-
-# SQL Injection Detection: Feature Extraction
-badwords = ['sleep', 'drop', 'uid', 'uname', 'select', 'waitfor', 'delay', 'system', 'union', 'order by', 'group by']
-
-def parse_raw_http_request(rawreq):
+# SQL Injection Detection: Feature Extraction (6 features)
+badwords = ['sleep', 'drop', 'uid', 'uname', 'select', 'waitfor', 'delay', 'system', 'union', 'order by', 'group by', 'insert', 'update', 'delete']
+sql_keywords = ['or', 'and', 'union', 'select', 'insert', 'update', 'delete']
+def extract_sql_features(input_data):
+    if not input_data or not isinstance(input_data, str):
+        return [0] * 6  # Return zeros for 6 features
     try:
-        raw = rawreq.decode('utf-8')
-    except:
-        raw = rawreq
-    headers = {}
-    sp = raw.split('\r\n\r\n', 1)
-    if len(sp) > 1 and sp[1] != "":
-        head = sp[0]
-        body = sp[1]
-    else:
-        head = sp[0]
-        body = ""
-    c1 = head.split('\n', head.count('\n'))
-    method = c1[0].split(' ', 2)[0]
-    path = c1[0].split(' ', 2)[1]
-    for i in range(1, head.count('\n') + 1):
-        slice1 = c1[i].split(': ', 1)
-        if slice1[0] != "":
-            try:
-                headers[slice1[0]] = slice1[1]
-            except:
-                pass
-    return headers, method, body, path
-
-def extract_sql_features(method, path_enc, body_enc, headers):
-    badwords_count = 0
-    path = unquote_plus(path_enc)
-    body = unquote(body_enc)
-    single_q = path.count("'") + body.count("'")
-    double_q = path.count('"') + body.count('"')
-    dashes = path.count("--") + body.count("--")
-    braces = path.count("(") + body.count("(")
-    spaces = path.count(" ") + body.count(" ")
-    for word in badwords:
-        badwords_count += path.count(word) + body.count(word)
-        for header in headers:
-            badwords_count += headers[header].count(word)
-    return [single_q, double_q, dashes, braces, spaces, badwords_count]
-
-# XSS Detection: Feature Extraction
+        input_data = unquote_plus(input_data.lower())
+        # Count quotes and braces only if near SQL keywords with stricter context
+        single_q = sum(1 for i, char in enumerate(input_data) if char == "'" and any(kw in input_data[max(0, i-10):i+10] for kw in sql_keywords))
+        double_q = sum(1 for i, char in enumerate(input_data) if char == '"' and any(kw in input_data[max(0, i-10):i+10] for kw in sql_keywords))
+        dashes = input_data.count("--") if any(kw in input_data for kw in sql_keywords) else 0
+        braces = sum(1 for i, char in enumerate(input_data) if char == "(" and any(kw in input_data[max(0, i-10):i+10] for kw in sql_keywords))
+        spaces = input_data.count(" ") if any(kw in input_data for kw in sql_keywords) else 0
+        badwords_count = sum(input_data.count(word) for word in badwords if word in input_data)
+        features = [single_q, double_q, dashes, braces, spaces, badwords_count]
+        print(f"SQL Features for '{input_data}': {features} (Length: {len(features)})")  # Debugging
+        return features
+    except Exception as e:
+        print(f"Error in SQL feature extraction: {e}")
+        return [0] * 6
+# XSS Detection: Advanced Feature Extraction (26 features)
 try:
     d2v_model = Doc2Vec.load("lib/d2v.model")
 except FileNotFoundError:
     print("Error: 'lib/d2v.model' not found. Please ensure it is in the 'lib' directory.")
     exit(1)
-
+except Exception as e:
+    print(f"Error loading Doc2Vec model: {e}")
+    exit(1)
 def getVec(text):
     features = []
     for line in text:
-        test_data = word_tokenize(line.lower())
-        v1 = d2v_model.infer_vector(test_data)
+        if not line or not isinstance(line, str):
+            test_data = ["default"]
+        else:
+            test_data = word_tokenize(unquote(line).lower())
+        try:
+            v1 = d2v_model.infer_vector(test_data)
+            if len(v1) != 20:
+                raise ValueError("Doc2Vec vector length mismatch")
+        except Exception as e:
+            print(f"Doc2Vec inference error: {e}")
+            v1 = [0] * 20  # Fallback vector, 20 dimensions
         featureVec = v1
-        lineDecode = unquote(line)
-        lowerStr = str(lineDecode).lower()
-        feature1 = sum(lowerStr.count(tag) for tag in ['link', 'object', 'form', 'embed', 'ilayer', 'layer', 'style', 'applet', 'meta', 'img', 'iframe', 'marquee'])
-        feature2 = sum(lowerStr.count(method) for method in ['exec', 'fromcharcode', 'eval', 'alert', 'getelementsbytagname', 'write', 'unescape', 'escape', 'prompt', 'onload', 'onclick', 'onerror', 'onpage', 'confirm'])
-        feature3 = lowerStr.count('.js')
-        feature4 = lowerStr.count('javascript')
-        feature5 = len(lowerStr)
-        feature6 = sum(lowerStr.count(script) for script in ['script', '<script', '%3cscript', '%3c%73%63%72%69%70%74'])
-        feature7 = sum(lowerStr.count(char) for char in ['&', '<', '>', '"', '\'', '/', '%', '*', ';', '+', '=', '%3C'])
-        feature8 = lowerStr.count('http')
-        featureVec = np.append(featureVec, [feature1, feature2, feature3, feature4, feature5, feature6, feature7, feature8])
-        features.append(featureVec)
+        try:
+            lineDecode = unquote(line)
+            lowerStr = str(lineDecode).lower()
+            # Focus on XSS-specific features
+            feature1 = sum(lowerStr.count(tag) for tag in ['script', '<script', 'iframe', 'onerror', 'onload'])
+            feature2 = sum(lowerStr.count(method) for method in ['alert', 'eval', 'exec', 'write', 'unescape'])
+            feature3 = lowerStr.count('.js')
+            feature4 = lowerStr.count('javascript')
+            feature5 = len(lowerStr) if feature1 > 0 or feature2 > 0 else 0  # Length only if XSS-like
+            feature6 = sum(1 for char in ['<', '>', '&'] if char in lowerStr and ('script' in lowerStr or 'javascript' in lowerStr))
+            feature_vec = np.append(featureVec, [feature1, feature2, feature3, feature4, feature5, feature6])
+            if len(feature_vec) != 26:
+                raise ValueError("XSS feature vector length mismatch")
+            features.append(feature_vec)
+            print(f"XSS Features for '{line}': {feature_vec} (Length: {len(feature_vec)})")  # Debugging
+        except Exception as e:
+            print(f"Error in XSS feature extraction: {e}")
+            features.append(np.append([0] * 20, [0] * 6))
     return features
-
 # Load or Train Models
 def load_or_train_sql_model():
     try:
-        with open("sql_injection_model.pkl", "rb") as f:
+        with open("sqli_model_test1.pkl", "rb") as f:
             sql_model = pickle.load(f)
             sql_scaler = pickle.load(f)
-    except (FileNotFoundError, EOFError):
-        if not os.path.exists("demo_good_and_bad_requests.csv"):
-            try:
-                good_df = pd.read_csv("demo_good_requests.csv")
-                bad_df = pd.read_csv("demo_bad_responses.csv")
-                combined_df = pd.concat([good_df, bad_df], ignore_index=True)
-                combined_df.to_csv("demo_good_and_bad_requests.csv", index=False)
-                print("Combined dataset created: demo_good_and_bad_requests.csv")
-                print("Class distribution:", combined_df['class'].value_counts())
-            except FileNotFoundError as e:
-                print(f"Error: {e}. Ensure 'demo_good_requests.csv' and 'demo_bad_responses.csv' are in the directory.")
-                exit(1)
-        
-        try:
-            df = pd.read_csv("demo_good_and_bad_requests.csv")
-        except FileNotFoundError:
-            print("Error: 'demo_good_and_bad_requests.csv' not found.")
+    except (FileNotFoundError, EOFError, Exception) as e:
+        if not os.path.exists("demo_good_and_bad_requests_test4.csv"):
+            print("Error: 'demo_good_and_bad_requests_test4.csv' not found. Ensure it exists in the directory.")
             exit(1)
         
-        print("Class distribution:", df['class'].value_counts())
-        
-        if len(df['class'].unique()) < 2:
-            raise ValueError("Dataset must contain both 'good' and 'bad' classes.")
-        
-        X = df[['single_q', 'double_q', 'dashes', 'braces', 'spaces', 'badwords']].values
-        y = df['class'].apply(lambda x: 1 if x.lower() == 'bad' else 0).values
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-        sql_scaler = StandardScaler()
-        X_train_scaled = sql_scaler.fit_transform(X_train)
-        X_test_scaled = sql_scaler.transform(X_test)
-        sql_model = LogisticRegression(random_state=42)
-        sql_model.fit(X_train_scaled, y_train)
-        print(f"SQL Model Accuracy: {accuracy_score(y_test, sql_model.predict(X_test_scaled))}")
-        with open("sql_injection_model.pkl", "wb") as f:
-            pickle.dump(sql_model, f)
-            pickle.dump(sql_scaler, f)
+        try:
+            df = pd.read_csv("demo_good_and_bad_requests_test4.csv")
+            print("Class distribution:", df['class'].value_counts())
+            
+            # Fix labeling if needed (example correction)
+            df['class'] = df['class'].apply(lambda x: 1 if x.lower() == 'bad' else 0)
+            if len(df['class'].unique()) < 2:
+                raise ValueError("Dataset must contain both 'good' and 'bad' classes.")
+            
+            X = df[['single_q', 'double_q', 'dashes', 'braces', 'spaces', 'badwords']].values
+            y = df['class'].values
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+            sql_scaler = StandardScaler()
+            X_train_scaled = sql_scaler.fit_transform(X_train)
+            X_test_scaled = sql_scaler.transform(X_test)
+            sql_model = LogisticRegression(random_state=42)
+            sql_model.fit(X_train_scaled, y_train)
+            print(f"SQL Model Accuracy: {accuracy_score(y_test, sql_model.predict(X_test_scaled))}")
+            with open("sqli_model_test1.pkl", "wb") as f:
+                pickle.dump(sql_model, f)
+                pickle.dump(sql_scaler, f)
+        except Exception as e:
+            print(f"Error training SQL model: {e}")
+            exit(1)
     return sql_model, sql_scaler
-
 def load_xss_model():
     try:
         with open("lib/RandomForestClassifier.sav", "rb") as f:
             xss_model = pickle.load(f)
+        if not hasattr(xss_model, 'estimators_') or xss_model.estimators_ is None:
+            raise ValueError("Loaded RandomForestClassifier is not fitted.")
+        return xss_model
     except FileNotFoundError:
         print("Error: 'lib/RandomForestClassifier.sav' not found. Please ensure it is in the 'lib' directory.")
         exit(1)
-    return xss_model
-
+    except Exception as e:
+        print(f"Error loading RandomForestClassifier: {e}")
+        exit(1)
 sql_model, sql_scaler = load_or_train_sql_model()
 xss_model = load_xss_model()
-
 # Serve the Front-End Website
 @app.route('/')
 def home():
-    return render_template('index.html')
-
+    try:
+        return render_template('index.html')
+    except Exception as e:
+        return jsonify({"error": f"Template error: {e}"}), 500
 # Real-Time Detection Endpoint (API)
 @app.route('/detect', methods=['GET', 'POST'])
 def detect():
     try:
         method = request.method
-        path = request.path
-        headers = {k: v for k, v in request.headers.items()}
-        body = request.get_data().decode('utf-8', errors='ignore')
-        raw_request = f"{method} {path} HTTP/1.1\r\n"
-        for key, value in headers.items():
-            raw_request += f"{key}: {value}\r\n"
-        raw_request += "\r\n" + body
-
-        headers, method, body, path = parse_raw_http_request(raw_request.encode('utf-8'))
-
+        if method == 'POST':
+            input_data = request.form.get('query', '') or request.form.get('cfile', '')
+        else:
+            input_data = request.args.get('query', '')
+        if not input_data:
+            raise ValueError("No input query provided")
+        
+        try:
+            decoded_data = unquote_plus(input_data)
+        except Exception as e:
+            decoded_data = input_data
+            print(f"Decoding error: {e}")
+        
         # SQL Injection Detection
-        sql_features = extract_sql_features(method, path, body, headers)
-        sql_features_scaled = sql_scaler.transform([sql_features])
-        sql_prediction = sql_model.predict(sql_features_scaled)[0]
-        sql_result = "SQL Injection Detected" if sql_prediction == 1 else "No SQL Injection"
-
+                # SQL Injection Detection
+        sql_features = extract_sql_features(decoded_data)
+        if len(sql_features) != 6:
+            raise ValueError(f"SQL features invalid: {len(sql_features)} features, expected 6")
+        
+        # ✅ Fix false positives: Skip prediction if all features are zero
+        if sum(sql_features) == 0:
+            sql_result = "No SQL Injection"
+            sql_prediction = 0
+        else:
+            sql_features_scaled = sql_scaler.transform([sql_features])
+            sql_prediction = sql_model.predict(sql_features_scaled)[0]
+            sql_result = "SQL Injection Detected" if sql_prediction == 1 else "No SQL Injection"
         # XSS Detection
-        query = body if body else path
-        xss_features = getVec([query])
+        xss_features = getVec([decoded_data])
+        if len(xss_features[0]) != 26:
+            raise ValueError(f"XSS features invalid: {len(xss_features[0])} features, expected 26")
         xss_prediction = xss_model.predict(xss_features)[0]
         xss_result = "XSS Detected" if xss_prediction == 1 else "No XSS"
-
-        log_entry = f"Request: {raw_request}\nSQL: {sql_result}\nXSS: {xss_result}\n\n"
+        # Log the request with detailed debug info
+        log_entry = f"Request Method: {method}\nInput Query: {input_data}\nSQL Features: {sql_features}\nSQL Prediction: {sql_prediction}\nXSS Features: {xss_features[0]}\nXSS Prediction: {xss_prediction}\nSQL Result: {sql_result}\nXSS Result: {xss_result}\n\n"
         with open("detection_log.txt", "a") as f:
             f.write(log_entry)
-
         return jsonify({"sql_injection": sql_result, "xss": xss_result})
     except Exception as e:
         error_msg = f"Error during detection: {str(e)}"
-        log_entry = f"Request: {raw_request}\nError: {error_msg}\n\n"
+        log_entry = f"Request Method: {method}\nInput Query: {input_data}\nError: {error_msg}\n\n"
         with open("detection_log.txt", "a") as f:
             f.write(log_entry)
-        return jsonify({"error": error_msg}), 500
-
+        return jsonify({"sql_injection": error_msg, "xss": error_msg}), 500
 # Handle favicon to avoid 404 errors
 @app.route('/favicon.ico')
 def favicon():
     return '', 204
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    try:
+        app.run(host='127.0.0.1', port=5000, debug=True)
+    except Exception as e:
+        print(f"Server error: {e}")
+        exit(1)
